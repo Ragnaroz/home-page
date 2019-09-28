9c66cce43614f00f2fa4b7ad6c20d6b0d5599d45
Ragnaroz <41728366+Ragnaroz@users.noreply.github.com>
2019-09-28 18:23:24 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,11 +2,6 @@ import Vue from 'vue'
 import HomePage from './HomePage.vue'
 import vueCustomElement from 'vue-custom-element';
 
-// new Vue({
-//   el: '#home-page',
-//   render: h => h(HomePage)
-// })
-
 Vue.config.ignoredElements = [
   'home-page'
 ];
